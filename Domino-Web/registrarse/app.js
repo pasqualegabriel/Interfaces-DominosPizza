b44cf30b27d611121c42e995fd69d8bfd73f540c
DominikowIvan <DominikowIvan@Gmail.com>
2017-10-15 18:34:08 -0300

    se termino Y Arreglo el ApiRest

---
+++ Domino-Web/registrarse/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = angular.module('registrarseApp', []);
+
+app.controller('UsuarioCtrl', function () {
+
+	this.userName = 'UsuarioGoku';
+
+	this.contrasenia = 'password';
+
+	this.mail = 'kakaroto@gmail.com';
+
+	this.direccion = 'vegita 123456';
+
+	this.nombre = 'Goku';
+
+});
