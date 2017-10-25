ca1839eb7e0c7388fb2b08a0899b4a575e723ae3
DominikowIvan <DominikowIvan@Gmail.com>
2017-10-25 15:00:14 -0300

    Se arreglaron los problemas de validaciones en iniciarSesion

--- Domino-Web/js/iniciarSesion.js
+++ Domino-Web/js/iniciarSesion.js
@@ -9,19 +9,32 @@ function SessionModel($state, userService) {
 
     this.nick = '';
     this.pass = '';
+    this.selectedUser= undefined;
 
     /*Protocolo */
     this.chequear = function() {
+        this.selectedUser= userService.getUserByNick(this.nick);
 
-        var aUser = userService.getUserByNick(this.nick);
+        if((this.validUserCredentials()) ){
+           // this.registrarInicio();
+            $state.go("pizzaSelector");
+        }
+    };
 
-        if(aUser === undefined){
-            alert("!El usuario no esta registrado¡");
-        }else if(aUser.password !== this.pass){
-            alert("!El password no es correcto¡");
-            }else{
-                $state.go("pizzaSelector");
-            }
+    this.validUserCredentials = function () {
+        return !this.userNotFound() && !this.incorrectPassword()
+    };
+
+   // this.registrarInicio = function () {
+   //     inicioSesionService.addLogginForUser(this.selectedUser)
+   // };
+
+    this.userNotFound = function() {
+      return this.selectedUser === undefined
+    };
+
+    this.incorrectPassword = function(){
+        return this.selectedUser.password !== this.pass
     };
 
     this.goRegister = function() {
